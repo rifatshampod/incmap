8d3e6fdf33d88680a46b442ac8f4b930114c96ab
rifatshampod <shampodrifat@gmail.com>
2021-05-18 01:15:03 +0800

    notification added using firebase cloud messaging

--- app/src/main/java/pwdmap/map/incmap/MapsFragment.java
+++ app/src/main/java/pwdmap/map/incmap/MapsFragment.java
@@ -16,6 +16,8 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+
+
 public class MapsFragment extends Fragment {
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
